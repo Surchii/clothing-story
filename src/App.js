c189e054acf5315be92acefe907848e9d21ba71f
Ahmed <Ahmed_surchi@hotmail.se>
2022-05-09 17:02:05 +0200

    Uppdaterat vår app med shop page och lagt till två andra componenter som collection item och collection preview

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.composnent";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
+import ShopPage from "./pages/shop/shop.componenet";
 
 function App() {
-   return (
-      <div>
-         <HomePage />
-      </div>
-   );
+  return (
+    <div>
+      <Switch>
+        <Route exact path={"/"} component={HomePage} />
+        <Route path={"/shop"} component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
