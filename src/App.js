688ee3228dbc6d00ae71da98a1f684d12de12494
Ahmed <Ahmed_surchi@hotmail.se>
2022-05-09 14:09:46 +0200

    Har minskat vår homepage till mindre componenter och fixat stylen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.composnent";
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+   return (
+      <div>
+         <HomePage />
+      </div>
+   );
 }
 
 export default App;
