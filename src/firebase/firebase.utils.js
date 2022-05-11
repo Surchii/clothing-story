6634ed2adbe65ee7dadeaf2a1db5908ff9075a5a
Ahmed <Ahmed_surchi@hotmail.se>
2022-05-11 03:07:44 +0200

    Har fixat firbase och skapade sign in med google sign in

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {
+  apiKey: "AIzaSyAlGzEFpA6sXB1t7cxVvkzIKBUDG6dWciE",
+  authDomain: "cloth-db-1d6a3.firebaseapp.com",
+  projectId: "cloth-db-1d6a3",
+  storageBucket: "cloth-db-1d6a3.appspot.com",
+  messagingSenderId: "887997556906",
+  appId: "1:887997556906:web:20164d21a1f7621c8a48d3",
+  measurementId: "G-VWTGH0S3J0",
+};
+
+firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
