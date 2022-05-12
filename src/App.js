32286cb3003643b15fd976b03e15c1f72e873776
Ahmed <Ahmed_surchi@hotmail.se>
2022-05-12 15:05:22 +0200

    Har impelmenterat firebase utils och har sorterad authenticated users, har även fixat sign up page också med dess styles.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,32 @@ import "./App.css";
 import ShopPage from "./pages/shop/shop.componenet";
 import SignInAndSignUpPage from "./pages/sign-in-and sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
-
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
    constructor() {
       super();
       this.state = {
-         currentUser: null
-      }
+         currentUser: null,
+      };
    }
    unsubscribeFromAuth = null;
    componentDidMount() {
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-         this.setState({ currentUser: user });
-         console.log(user);
-      })
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+         if (userAuth) {
+            const userRef = await createUserProfileDocument(userAuth);
+            userRef.onSnapshot((snapShot) => {
+               this.setState({
+                  currentUser: {
+                     id: snapShot.id,
+                     ...snapShot.data(),
+                  },
+               });
+               console.log(this.state);
+            });
+         }
+         this.setState({ currentUser: userAuth });
+      });
    }
    componentWillUnmount() {
       this.unsubscribeFromAuth();
@@ -34,7 +44,6 @@ class App extends React.Component {
                <Route exact path={"/"} component={HomePage} />
                <Route path={"/shop"} component={ShopPage} />
                <Route path={"/signin"} component={SignInAndSignUpPage} />
-
             </Switch>
          </div>
       );
