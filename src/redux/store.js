e32e33acacfd6517fecb97911b562bd8f220ee04
Ahmed <Ahmed_surchi@hotmail.se>
2022-05-23 15:27:43 +0200

    Har addat styled component librare och har ändrat header och customButton component styles till nya styled compoeneten

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,7 @@ import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [];
+const middlewares = [logger];
 
 if (process.env.NODE_ENV === 'development') {
    middlewares.push(logger);
@@ -16,3 +16,4 @@ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
 
 export default { store, persistStore };
+
