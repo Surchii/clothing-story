ac0baaf93ba16c248fb03e07aad3f375e9952535
Ahmed <Ahmed_surchi@hotmail.se>
2022-07-17 01:25:17 +0200

    Uppdaterar vår application App och använt useEffect

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -12,6 +12,7 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
+
   return (
     <CollectionPageContainer>
       <CollectionTitle>{title}</CollectionTitle>
